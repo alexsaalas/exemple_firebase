1920852fadc4bf6f09e5e9ee1005adb99a45b240
alexsalas <alexsalas@ceroca.cat>
2025-02-12 16:00:57 +0100

    Pagina registre front end fet

--- lib/components/boto_auth.dart
+++ lib/components/boto_auth.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
 class BotoAuth extends StatelessWidget {
-  const BotoAuth({super.key});
+  final String text;
+  final Function() onTap;
+
+  const BotoAuth({
+    super.key, 
+    required this.text, 
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text("TextButo"),
+    return GestureDetector(
+      onTap: onTap, // Correctly passing the onTap function
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: const Color.fromARGB(255, 244, 111, 54),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Text(
+            text, // No longer needs the 'const' since the text is dynamic
+            style: const TextStyle(
+              color: Colors.orange,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
